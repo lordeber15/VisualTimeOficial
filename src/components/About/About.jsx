204e7e416b9238dcceaf9f75932a162354430ce2
lordeber15 <eberpalomino@hotmail.com>
2023-08-18 15:39:50 -0500

    casi final

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,27 @@
 import style from "./About.module.css";
-
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Heading,
+} from "@chakra-ui/react";
 function About() {
-  return <section className={style.container}>About</section>;
+  return (
+    <section className={style.container}>
+      <div className={style.form}>
+        <Heading>Contactanos</Heading>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+          <FormLabel>Telefono</FormLabel>
+          <Input type="number" />
+          <FormLabel>Mensaje</FormLabel>
+          <Textarea />
+        </FormControl>
+      </div>
+    </section>
+  );
 }
 
 export default About;
